a43b488221215795d5cb95f86ac8c00e9b1e8caf
Massa Timothée <timothee.massa.perso@gmail.com>
2023-09-02 18:37:57 +0200

    Header finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <header>
-            <h1>LetterWatchd</h1>
+            <h1 className='header--title'>Letterwatchd</h1>
         </header>
     )
 }
