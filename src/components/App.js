1c8a518c4c89da25dbb396456e43f15ebc4fd066
Massa Timothée <timothee.massa.perso@gmail.com>
2023-10-25 16:06:00 +0200

    Login form improved and username is now stored

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,68 @@
-import React, { useState} from 'react';
-import '../style.css';
-import Header from './Header';
-import Button from './Button';
-import Card from './Card';
-import Login from './Login';
-import filmData from '../watchlist/data';
-import { getRandomIndex } from '../functions/utils';
+import React, { useState } from "react";
+import "../style.css";
+import Header from "./Header";
+import Button from "./Button";
+import Card from "./Card";
+import Login from "./Login";
+import filmData from "../watchlist/data";
+import { getRandomIndex } from "../functions/utils";
 
 export default function App() {
-    const [isCardVisible, setCardVisible] = useState(false);
-    const [isLoginVisible, setLoginVisible] = useState(true);
-    const [randomIndex, setRandomIndex] = useState(null);
+  const [isCardVisible, setCardVisible] = useState(false);
+  const [isLoginVisible, setLoginVisible] = useState(true);
+  const [randomIndex, setRandomIndex] = useState(null);
+  const [username, setUsername] = useState(""); 
 
-    const toggleOverlay = () => {
-        setCardVisible(!isCardVisible);
-    };
+  const toggleOverlay = () => {
+    setCardVisible(!isCardVisible);
+  };
 
-    const toggleLogin = () => {
-        setLoginVisible(!isLoginVisible);
-    }
+  const toggleLogin = () => {
+    setLoginVisible(!isLoginVisible);
+  };
 
-    const handleClick = () => {
-        const newIndex = getRandomIndex(filmData.length);
-        setRandomIndex(newIndex);
-        toggleOverlay();
-    }
+  const handleUsernameSubmit = (newUsername) => {
+    setUsername(newUsername);
+    toggleLogin();
+  };
 
-    const resetState = () => {
-        setCardVisible(false);
-        setRandomIndex(null);
-    }
+  const handleClick = () => {
+    const newIndex = getRandomIndex(filmData.length);
+    setRandomIndex(newIndex);
+    toggleOverlay();
+  };
 
-    return (
-        <div className='app'>
-            <Header />
-            <Login />
-            <Button onToggleOverlay={handleClick} />
-            {isCardVisible && (
-                <div className="overlay">
-                    <section className={`card--container ${isCardVisible ? 'card-visible' : ''}`}>
-                        {randomIndex !== null && (
-                            <div>
-                                <Card key={filmData[randomIndex].id} {...filmData[randomIndex]}
-                                    resetState={resetState} />
-                            </div>
-                        )}
-                    </section>
-                </div>
-            )}
+  const resetState = () => {
+    setCardVisible(false);
+    setRandomIndex(null);
+  };
+
+  return (
+    <div className="app">
+      <Header />
+      {isLoginVisible && (
+        <div>
+          <Login onUsernameSubmit={handleUsernameSubmit} />
         </div>
-    )
+      )}
+      <Button onToggleOverlay={handleClick} />
+      {isCardVisible && (
+        <div className="overlay">
+          <section
+            className={`card--container ${isCardVisible ? "card-visible" : ""}`}
+          >
+            {randomIndex !== null && (
+              <div>
+                <Card
+                  key={filmData[randomIndex].id}
+                  {...filmData[randomIndex]}
+                  resetState={resetState}
+                />
+              </div>
+            )}
+          </section>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Login() {
-    return (
-        <div className="login--container">
-            <h2 className='login--title'>Letterboxd username?</h2>
-            <input className='login--input-username' required autoFocus></input>
-            <button className='login--button' type='submit'>Submit</button>
-        </div>
-    )
+export default function Login({ onUsernameSubmit }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    onUsernameSubmit(inputValue);
+  };
+
+  return (
+    <div className="login--container">
+      <h2 className='login--title'>Letterboxd username?</h2>
+      <input
+        className='login--input-username'
+        required
+        autoFocus
+        value={inputValue}
+        onChange={handleInputChange}
+      ></input>
+      <button className='submit-form--button' onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
